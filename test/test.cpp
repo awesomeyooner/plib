33056dfb3286207211347a175a2648156572c141
Susan Yoon <awesomeyooner@gmail.com>
2025-12-27 13:08:41 -0800

    working example of imgui and implot

--- test/test.cpp
+++ test/test.cpp
@@ -1,16 +1,113 @@
 #include <iostream>
+#include <cstdio>
 
 #include "plib/util/util.hpp"
 #include "plib/i2c/i2c.hpp"
 #include "plib/i2c/wire_device.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_sdl2.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+
+#include "implot/implot.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
 int main()
 {
-    util::print("Hello World!");
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+    {
+        std::printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
 
-    auto input = util::get_user_input_double("Enter a number: ", true, "Sorry, your input was not a number. Please enter a number: ");
+    // OpenGL Settings
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
-    util::print(input.value);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    // Create Initial Window
+    SDL_Window* window = SDL_CreateWindow(
+        "My Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    // Create Context
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // vsync
+
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImGui::StyleColorsDark();
+
+    // Initialize SDL2 and OpenGL for ImGui
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Keep alive
+    
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello");
+        ImGui::Text("ImGui + SDL2 + OpenGL works!");
+
+        double x_data[4] = {1, 2, 3, 4};
+        double y_data[4] = {1, 2, 3, 4};
+
+        if(ImPlot::BeginPlot("My Plot"))
+        {
+            ImPlot::PlotLine("My Line Plot", x_data, y_data, 4);
+            ImPlot::EndPlot();
+        }
+
+
+        ImGui::End();
+
+        ImGui::Render();
+
+        glViewport(0, 0, 800, 600);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    ImPlot::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }

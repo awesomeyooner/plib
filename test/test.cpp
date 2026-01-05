4895ef82daa61a7c50c0a09ac05f087caa7fc907
Susan Yoon <awesomeyooner@gmail.com>
2026-01-05 00:54:08 -0800

    added sliders and textbox input for modifying cubic hermite spline inputs

--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstdio>
+#include <type_traits>
 
 #include "plib/util/util.hpp"
 #include "plib/util/system.hpp"
 #include "plib/util/implot_plotter.hpp"
 
 #include "plib/math/motion/cubic_hermite_spline.hpp"
-#include "plib/math/motion/my_class.hpp"
 
 #include "plib/i2c/i2c.hpp"
 #include "plib/i2c/wire_device.hpp"
@@ -49,20 +49,55 @@ void test_spline()
 {
     ImPlotter::initialize();
 
-    // CubicHermiteSpline spline;
+    CubicHermiteSpline spline;
 
-    // double bob = spline.k;
+    double* P0 = &spline.m_P0;
+    double* v0 = &spline.m_v0;
+    double* P1 = &spline.m_P1;
+    double* v1 = &spline.m_v1;
+
+    double* a_max = &spline.m_a_max;
+    double* v_max = &spline.m_v_max;
+
+    double slider_min = -50;
+    double slider_max = 50;
 
     while (System::is_alive())
     {
         std::function<status_utils::StatusCode(SDL_Window*, SDL_GLContext&)> runnable;
 
         runnable = 
-            [](SDL_Window* window, SDL_GLContext& context){
+            [&spline, P0, v0, P1, v1, a_max, v_max, slider_min, slider_max](SDL_Window* window, SDL_GLContext& context){
 
-                double P0, v0, P1, v1, ka, kv, k;
+                ImGui::InputDouble("P0 Input", P0);
+                ImGui::SliderScalar("P0 Slider", ImGuiDataType_Double, P0, &slider_min, & slider_max, "%.1f Units");
 
-                ImGui::InputDouble("P0", &P0);
+                ImGui::InputDouble("v0 Input", v0);
+                ImGui::SliderScalar("v0 Slider", ImGuiDataType_Double, v0, &slider_min, & slider_max, "%.1f Units");
+
+                ImGui::InputDouble("P1 Input", P1);
+                ImGui::SliderScalar("P1 Slider", ImGuiDataType_Double, P1, &slider_min, & slider_max, "%.1f Units");
+
+                ImGui::InputDouble("v1 Input", v1);
+                ImGui::SliderScalar("v1 Slider", ImGuiDataType_Double, v1, &slider_min, & slider_max, "%.1f Units");
+
+                ImGui::InputDouble("a_max Input", a_max);
+                ImGui::SliderScalar("a_max Slider", ImGuiDataType_Double, a_max, &slider_min, & slider_max, "%.1f Units");
+
+                ImGui::InputDouble("v_max Input", v_max);
+                ImGui::SliderScalar("v_max Slider", ImGuiDataType_Double, v_max, &slider_min, & slider_max, "%.1f Units");
+
+                double ka = spline.get_ka();
+                double kv = spline.get_kv();
+                double k = spline.get_k();
+
+                std::string ka_text = "ka: " + std::to_string(ka);
+                std::string kv_text = "kv: " + std::to_string(kv);
+                std::string k_text = "k: " + std::to_string(k);
+                
+                ImGui::Text(ka_text.c_str());
+                ImGui::Text(kv_text.c_str());
+                ImGui::Text(k_text.c_str());
 
                 if(ImPlot::BeginPlot("My Plot", ImVec2(-1, -1)))
                 {
@@ -92,9 +127,8 @@ void test_spline()
 
 int main()
 {
-    // test_spline();
-
-    Logger::info(MyClass::my_function());
+    test_spline();
 
     return 0;
+
 } // end of "main"

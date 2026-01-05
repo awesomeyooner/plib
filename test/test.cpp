52ba229fba3af13149997aeba221a0a654b5f3f6
Susan Yoon <awesomeyooner@gmail.com>
2026-01-05 01:17:32 -0800

    added test for displaying cubic hermite spline

--- test/test.cpp
+++ test/test.cpp
@@ -62,12 +62,14 @@ void test_spline()
     double slider_min = -50;
     double slider_max = 50;
 
+    int resolution = 1000;
+
     while (System::is_alive())
     {
         std::function<status_utils::StatusCode(SDL_Window*, SDL_GLContext&)> runnable;
 
         runnable = 
-            [&spline, P0, v0, P1, v1, a_max, v_max, slider_min, slider_max](SDL_Window* window, SDL_GLContext& context){
+            [&spline, P0, v0, P1, v1, a_max, v_max, slider_min, slider_max, &resolution](SDL_Window* window, SDL_GLContext& context){
 
                 ImGui::InputDouble("P0 Input", P0);
                 ImGui::SliderScalar("P0 Slider", ImGuiDataType_Double, P0, &slider_min, & slider_max, "%.1f Units");
@@ -87,17 +89,22 @@ void test_spline()
                 ImGui::InputDouble("v_max Input", v_max);
                 ImGui::SliderScalar("v_max Slider", ImGuiDataType_Double, v_max, &slider_min, & slider_max, "%.1f Units");
 
+                ImGui::SliderInt("Resolution", &resolution, 100, 5000, "%d Units");
+
                 double ka = spline.get_ka();
                 double kv = spline.get_kv();
                 double k = spline.get_k();
+                double total_time = spline.get_total_time();
 
                 std::string ka_text = "ka: " + std::to_string(ka);
                 std::string kv_text = "kv: " + std::to_string(kv);
                 std::string k_text = "k: " + std::to_string(k);
+                std::string total_time_text = "Total Time: " + std::to_string(total_time);
                 
                 ImGui::Text(ka_text.c_str());
                 ImGui::Text(kv_text.c_str());
                 ImGui::Text(k_text.c_str());
+                ImGui::Text(total_time_text.c_str());
 
                 if(ImPlot::BeginPlot("My Plot", ImVec2(-1, -1)))
                 {
@@ -105,8 +112,21 @@ void test_spline()
                     ImPlot::SetupAxes("X Axis", "Y Axis", ImPlotAxisFlags_PanStretch, ImPlotAxisFlags_PanStretch);
                     // ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
 
-                    std::vector<double> data_x = {1, 2, 3, 4};
-                    std::vector<double> data_y = {1, 2, 3, 4};
+                    std::vector<double> data_x;
+                    std::vector<double> data_y;
+
+                    for (size_t i = 0; i < resolution; i++)
+                    {
+                        double time = ( (double)i / (double)resolution ) * spline.get_total_time();
+                        double output = spline.P(time);
+
+                        data_x.push_back(time);
+                        data_y.push_back(output);
+
+                    }
+
+                    Logger::debug(data_x.at(resolution - 1));
+                    Logger::debug(data_y.at(resolution - 1));
 
                     ImPlot::PlotLine("My Plot", data_x.data(), data_y.data(), data_x.size());
                         

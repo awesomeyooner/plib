888b69ac2809e96abfff93a0886309b194fe279f
Susan Yoon <awesomeyooner@gmail.com>
2026-01-05 16:20:45 -0800

    added helper struct for managing timestamps and data

--- include/plib/math/control/pid_controller.hpp
+++ include/plib/math/control/pid_controller.hpp
@@ -2,7 +2,11 @@
 #define PID_CONTROLLER_HPP
 
 #include <vector>
+#include <iostream>
 
+#include "plib/util/system.hpp"
+
+// Options for what Feed Forward method to use
 enum class FeedForwardType{
     
     // Applies a constant feedforward, regardless of direction
@@ -19,6 +23,47 @@ enum class FeedForwardType{
 
 }; // enum FeedForwardType
 
+
+// Struct for a value to be shared with its timestamp since it was updated / created
+
+template <typename T>
+struct TimestampedValue{
+    
+    T m_value;
+    double m_timestamp;
+
+    TimestampedValue(T value, double timestamp)
+    {
+        update(value, timestamp);
+
+    } // end of TimestampedValue
+
+    TimestampedValue(T value)
+    {
+        update(value);
+
+    } // end of TimestampedValue
+
+    // One-liner for updating the values
+    void update(T value, double timestamp)
+    {
+        m_value = value;
+        m_timestamp = timestamp
+
+    } // end of update
+
+    // Updates the value and auto refreshes the timestamp
+    void update(T value)
+    {
+        // Get the current time
+        double time = System::get_time_since_start();
+        update(value, time);
+
+    } // end of "update"
+
+}; // struct TimestampedValue
+
+// Class for a PID Controller, implementing feedforwards in addition to PID.
 class PIDController{
 
     public:
@@ -32,8 +77,11 @@ class PIDController{
         // Derivative Gain
         double m_kD;
 
-        // Feedforward Gain. Type is determined by `m_feedforward_type`
+        // Feedforward Gain. Type is determined by `m_ff_type`
         double m_kF;
+
+        // The type of feedforward to use
+        FeedForwardType m_ff_type;
 
         // The setpoint to go towards
         double m_setpoint;
@@ -45,10 +93,16 @@ class PIDController{
         double m_velocity;
 
         // The accumulated error buffer
-        std::vector<double> accumulated_error;
+        std::vector<double> error_buffer;
+
+        // How much time between the current and last integrated value to use
+        double integral_time_bound;
 
         // The timestamp of the previous loop
         double m_previous_timestamp;
+
+        PIDController(double kP, double kI, double kD);
+        PIDController(double kP, double kI, double kD, double kF, FeedForwardType ff_type);
 
         double get_error(double setpoint);
         double get_accumulated_error();
@@ -59,7 +113,8 @@ class PIDController{
         double calculate(double position, double setpoint, double velocity);
 
     private:
-
+        
+        double accumulated_error;
 
 
 }; // class PIDController
--- src/math/control/pid_controller.cpp
+++ src/math/control/pid_controller.cpp
@@ -1,1 +1,14 @@
 #include "plib/math/control/pid_controller.hpp"
+
+PIDController::PIDController(double kP, double kI, double kD, double kF, FeedForwardType ff_type)
+{
+    m_kP = kP;
+    m_kI = kI;
+    m_kD = kD;
+    m_kF = kF;
+    m_ff_type = ff_type;
+
+} // end of "PIDController"
+
+
+PIDController::PIDController(double kP, double kI, double kD) : PIDController(kP, kI, kD, 0, FeedForwardType::STATIC){}
